8acb5021ceeddc826410797a2f0373f210268eba
uchexm <109084069+uchexm@users.noreply.github.com>
2023-11-03 17:34:34 +0200

    Create Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "@/components/Hero";
 import Game from "@/components/Game";
+import Card from "@/components/Card";
+import GamesGrid from "@/components/GameGrid";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
       <Game />
+      <Card />
+      <GamesGrid />
     </div>
   );
 }
